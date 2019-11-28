327369af158ae0ee99912bca9ef8c5bc23462faf
Laasri Nadia <laasrinadiaa@gmail.com>
2019-11-28 23:12:18 +0100

    header paragraph fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,28 @@ import { ReactComponent as Logo } from '../utils/logo.svg';
 export default class Header extends Component {
   render() {
     return (
-      <div className='clip_path'>
+      <div className='Header'>
+        <div className='clip_path'></div>
         <div className='header_container'>
           <div className='left'>
             <ul>
               <li>
-                <a href='/'>Home</a>{' '}
+                <a href='/'>Home</a>
               </li>
 
               <li>
-                <a href='/'>Memes</a>{' '}
+                <a href='/'>Memes</a>
               </li>
 
               <li>
-                <a href='/'>About us</a>{' '}
+                <a href='/'>About us</a>
               </li>
             </ul>
 
             <div className='title_container'>
               <b>Generate Your</b> <span className='ownmeme'>Own Memes</span>
               <button>
-                <span>Make Memes</span>{' '}
+                <span>Make Memes</span>
               </button>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import Layout from '../utils/Layout';
+import Card from '../utils/card';
 export default class Home extends Component {
+  state = {
+    cards: [
+      {
+        title: 'Picture',
+        action: 'Choose a meme picture'
+      },
+      {
+        title: 'Text',
+        action: 'Add text to it'
+      },
+      {
+        title: 'Upload',
+        action: 'Your meme is ready to use'
+      }
+    ]
+  };
+
+  displayCards = () => {
+    this.state.cards.map(card => <Card />);
+  };
+
   render() {
-    return <Layout>Home</Layout>;
+    return (
+      <Layout>
+        <div className='home_wrapper'>
+          {' '}
+          {this.displayCards()}
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nostrum
+          deserunt eligendi nemo hic iure quae labore exercitationem minima
+          beatae.
+        </div>
+      </Layout>
+    );
   }
 }
