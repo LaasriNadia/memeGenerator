981fb70b64c6396593a4f2eda57f31966a8e689f
Laasri Nadia <laasrinadiaa@gmail.com>
2019-11-30 00:39:04 +0100

    material ui cards

--- src/utils/card.jsx
+++ src/utils/card.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const card = props => {
+const myCard = props => {
+  const bull = (
+    <span
+      className={{
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)'
+      }}
+    >
+      •
+    </span>
+  );
+
   return (
-    <div className='card'>
-      <h1>props.title</h1>
-      <h2>props.text</h2>
-    </div>
+    <Card className={{ minWidth: 275, boxShadow: '0 10px 10px #eee' }}>
+      <CardContent>
+        <Typography
+          className={{ fontSize: 14 }}
+          color='textSecondary'
+          gutterBottom
+        >
+          Word of the Day
+        </Typography>
+        <Typography variant='h5' component='h2'>
+          be
+          {bull}
+          nev
+          {bull}o{bull}
+          lent
+        </Typography>
+        <Typography className={{ marginBottom: 12 }} color='textSecondary'>
+          adjective
+        </Typography>
+        <Typography variant='body2' component='p'>
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
-export default card;
+
+export default myCard;
