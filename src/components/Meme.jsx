1b6022143c5c10cc914f55d55c9ff17996c4c59a
Laasri Nadia <laasrinadiaa@gmail.com>
2019-12-07 21:56:38 +0100

    adding download button

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ReactComponent as Slide } from '../utils/slide.svg';
 
+import html2canvas from 'html2canvas';
 export default class Meme extends Component {
   state = {
     topText: '',
@@ -41,6 +42,21 @@ export default class Meme extends Component {
     });
   };
 
+  test = e => {
+    e.preventDefault();
+    let object = this.refs.memeref;
+    // console.log(object);
+    html2canvas(object, {
+      logging: true,
+      letterRendering: 1,
+      allowTaint: true,
+      useCORS: true
+    }).then(canvas => {
+      var imgData = canvas.toDataURL('image/jpeg');
+      console.log(imgData);
+    });
+  };
+
   render() {
     return (
       <>
@@ -49,10 +65,11 @@ export default class Meme extends Component {
         <div className='meme_container'>
           <Slide className='memeSvg' />
           <div className='meme'>
-            <img src={this.state.randomImg} alt='' />
+            <img src={this.state.randomImg} alt='Meme' ref='memeref' />
             <h2 className='top'>{this.state.topText}</h2>
             <h2 className='bottom'>{this.state.bottomText}</h2>
           </div>
+
           <div className='form'>
             <form>
               <input
@@ -70,6 +87,7 @@ export default class Meme extends Component {
               <button onClick={e => this.generatePic(e)}>
                 Choose another picture
               </button>
+              <button onClick={e => this.test(e)}>Download</button>
             </form>
           </div>
         </div>
